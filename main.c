26b07bbb7e2596bc21f8f68a9ac265c1ab16f5b0
matthiasd05 <148857028+matthiasd05@users.noreply.github.com>
2024-03-11 16:44:30 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,7 @@
+#include<allegro.h>			// inclusion de la librairie
+int main()
+{
+    allegro_init();			//macro d’initialisation
+    allegro_message("OK ça marche") ;
+    return 0;
+} END_OF_MAIN() ;  		//macro de fin
